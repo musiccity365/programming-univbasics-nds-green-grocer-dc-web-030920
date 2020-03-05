d43ceff768a8b4385fc532b3c2a66376c72701a7
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-05 03:13:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -97,4 +97,8 @@ def checkout(cart, coupons)
     total += final_cart[counter][:price] + final_cart[counter][:count]
     counter += 1
   end
+  if total > 100
+    total -= (total * 0.10)
+  end
+  total
 end
